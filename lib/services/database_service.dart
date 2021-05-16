c9de203b2a8c573a527a09785fd2462c9ac436d0
Ali Daoud <ali.daoud08@outlook.sa>
2021-05-16 04:39:14 +0300

    limit quiz questions to get random 5 from 25

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,4 +1,4 @@
-//import 'dart:math';
+import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:quiz_app/models/question.dart';
@@ -9,18 +9,18 @@ class DBService {
   static const _COL_QUESTIONS = "questions";
 
   static const QUIZ_QUESTIONS_COUNT = 5;
-  static const QUESTIONS_COUNT = 5;
+  static const QUESTIONS_COUNT = 25;
 
   Future<List<Question>> getRandomQuestions() async {
     print("$TAG getting questions from database..");
-    // Random random = new Random();
-    // int randomNumber = random.nextInt(QUESTIONS_COUNT - QUIZ_QUESTIONS_COUNT);
-    int randomNumber =
-        0; //todo un comment the lines above instead to retrieve random questions after adding more questions to db
+    Random random = new Random();
+    int randomNumber = random.nextInt(QUESTIONS_COUNT - QUIZ_QUESTIONS_COUNT);
+    // int randomNumber = 0;
     List<Question> questions;
     await _dbRef
         .collection(_COL_QUESTIONS)
         .where("id", isGreaterThanOrEqualTo: randomNumber)
+        .limit(5)
         .get()
         .then((value) => questions = questionsFromSnapshot(value));
     return questions;
